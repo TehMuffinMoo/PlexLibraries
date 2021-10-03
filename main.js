f4d97544ac27bd5fba8eaf9fdcc169723a3ec4b2
causefx <causefx@me.com>
2021-10-03 00:07:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,53 +6,74 @@ $('body').arrive('#activeInfo', {onceOnly: true}, function() {
 });
 // FUNCTIONS
 function plexLibrariesPluginLaunch(){
-	var menuList = `<li><a class="inline-popups plexLibrariesModal" href="#plexLibraries-area" data-effect="mfp-zoom-out"><i class="fa fa-tv fa-fw"></i> <span lang="en">Plex Libraries</span></a></li>`;
-	var htmlDOM = `
-	<div id="plexLibraries-area" class="white-popup mfp-with-anim mfp-hide">
-		<div class="col-md-4 col-md-offset-4">
-			<div class="panel bg-org panel-info">
-				<div class="panel-heading">
-					<span lang="en">Customise Plex Libraries</span>
-				</div>
-				<div class="panel-body">
-					<div id="plexLibrariesTable">
-						<div class="white-box m-b-0">
-							<div class="col-12" style="width: 1514px">
-								<select name="plexUsers" id="plexUsers" style="width: 26%;display:none">
-										<option value="">- Plex User -</option>
-								</select><br>
+	organizrAPI2('GET','api/v2/plugins/plexlibraries/launch').success(function(data) {
+		try {
+			var menuList = `<li><a class="inline-popups plexLibrariesModal" href="#plexLibraries-area" data-effect="mfp-zoom-out"><i class="fa fa-tv fa-fw"></i> <span lang="en">Plex Libraries</span></a></li>`;
+			var htmlDOM = `
+				<div id="plexLibraries-area" class="white-popup mfp-with-anim mfp-hide">
+					<div class="col-md-4 col-md-offset-4">
+						<div class="panel bg-org panel-info">
+							<div class="panel-heading">
+								<span lang="en">Customise Plex Libraries</span>
 							</div>
-							<div class="user-btm-box" id="plexLibraries">
+							<div class="panel-body">
+								<div id="plexLibrariesTable">
+									<div class="white-box m-b-0">
+										<div class="row">
+											<div class="col-lg-12">
+												<select class="form-control" name="plexUsers" id="plexUsers" style="display:none">
+													<option value="">Choose a User</option>
+												</select><br>
+											</div>
+										</div>
+										<div class="table-responsive plexLibrariesTableList hidden">
+											<table class="table color-bordered-table purple-bordered-table">
+												<thead>
+													<tr>
+														<th width="20">Type</th>
+														<th>Name</th>
+														<th width="20">Action</th>
+													</tr>
+												</thead>
+												<tbody id="plexLibraries"></tbody>
+											</table>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
-	`;
-	$('.append-menu').after(menuList);
-	$('.organizr-area').after(htmlDOM);
+				`;
+			$('.append-menu').after(menuList);
+			$('.organizr-area').after(htmlDOM);
+		}catch(e) {
+			organizrCatchError(e,data);
+		}
+	}).fail(function(xhr) {
+		OrganizrApiError(xhr);
+	});
+
 }
 
 function plexLibrariesPluginLoadShares(){
 	$.getJSON( "api/v2/plugins/plexlibraries/shares", function( data ) {
 		$(function() {
 			// Single Response
-			if (data.response.data.SharedServer.Section) {
-				$.each(data.response.data.SharedServer.Section, function(_, obj) {
+			if (data.response.data.plexAdmin == false) {
+				$.each(data.response.data.libraryData, function(_, obj) {
 						plexLibrariesPluginLoadSharesItem(obj,"","","");
 				});
 			} else {
 				// Plex Admin response contains all users shares, mark all toggles as disabled whilst this is a work in progress.
-				$.each(data.response.data.SharedServer, function(_, sharedServer) {
+				$.each(data.response.data.libraryData, function(_, sharedServer) {
 					const thtml = $("#plexUsers ");
 					var dropdown = document.getElementById('plexUsers');
 					dropdown.style.display = "block";
-					var username = sharedServer['@attributes'].username;
-					var userId = sharedServer['@attributes'].id;
+					var username = sharedServer.username;
+					var userId = sharedServer.id;
 					thtml.append('<option value="'+username+'">'+username+'</option>');
-					$.each(sharedServer.Section, function(_, obj) {
+					$.each(sharedServer.libraries, function(_, obj) {
 						plexLibrariesPluginLoadSharesItem(obj,"",username,userId);
 					});
 				});
@@ -66,8 +87,8 @@ function plexLibrariesPluginLoadShares(){
 
 function plexLibrariesPluginLoadSharesItem(obj,disabled,username,userId){
 	const thtml = $("#plexLibraries ");
-	var mediaType = obj['@attributes'].type
-	var mediaShared = obj['@attributes'].shared
+	var mediaType = obj.type
+	var mediaShared = obj.shared
 	var mediaIcon = "0"
 	var checked = "";
 	switch(mediaType) {
@@ -97,23 +118,24 @@ function plexLibrariesPluginLoadSharesItem(obj,disabled,username,userId){
 	} else {
 		var display = "none"
 	}
-	$.each(obj, function(_, text) {
-		thtml.append('\
-		<div class="col-md-3 col-xs-6 p-l-0 p-r-0 text-center plexUser ' + username + '" style="display:' + display + '">\
-			<p class="text-' + mediaIconColour + '"><i class="ti-' + mediaIcon + ' fa-2x"></i></p>\
-			<h4 class="">' + obj['@attributes'].title + '</h4>\
-			<input type="checkbox" class="js-switch plexLibraries" data-size="small" data-color="#99d683" data-secondary-color="#f96262" value="' + obj['@attributes'].id + ':' + userId + '" ' + checked + ' ' + disabled +'>\
-		</div>\
-		');
-	});
+	let libItem = `
+		<tr class="plexUser ${username}" style="display: ${display}">
+			<td><p class="text-${mediaIconColour}"><i class="ti-${mediaIcon} fa-2x"></i></p></td>
+			<td>${obj.title}</td>
+			<td><input type="checkbox" class="js-switch plexLibraries" data-size="small" data-color="#99d683" data-secondary-color="#f96262" data-user-id="${userId}" value="${obj.id}" ${checked} ${disabled}></td>
+		</tr>
+	`;
+		thtml.append(libItem);
+
 }
 
 function onToggle() {
     $('.plexLibraries').change(function () {
+    	let userId = $(this).attr('data-user-id');
         if (this.checked) {
-            updateStatus(this.value,"checked");
+	        plexLibrariesPluginUpdateShare(userId,"share", this.value);
         } else {
-            updateStatus(this.value,"unchecked");
+	        plexLibrariesPluginUpdateShare(userId,"unshare", this.value);
         }
     });
 }
@@ -127,20 +149,37 @@ function onSelect() {
 		);
 		Array.from(document.getElementsByClassName(this.value)).forEach(
 			function(element, index, array) {
-				element.style.display = "block";
+				element.style.display = "table-row";
 			}
 		);
+		if($('.plexLibrariesTableList').hasClass('hidden')){
+			$('.plexLibrariesTableList').removeClass('hidden');
+		}
     });
 }
 
-function updateStatus(value,checked) {
-    $.ajax({
-        type: "POST",
-        url: "api/v2/plugins/plexlibraries/shares",
-        data: {toggle_update: true, shareData: value, checked: checked},
-        success: function (result) {
-            console.log(result);
-        }
-    });
+function plexLibrariesPluginUpdateShare(userId, action, shareId) {
+
+	$('#plexLibrariesTable').block({
+		message: '<h4><img src="plugins/images/busy.gif" /> Just a moment...</h4>',
+		css: {
+			border: '1px solid #000',
+			color: '#fff',
+			background: '#1b1b1b'
+		}
+	});
+	organizrAPI2('POST','api/v2/plugins/plexlibraries/shares/' + userId + '/' + action + '/' + shareId, {}).success(function(data) {
+		try {
+			let response = data.response;
+			$('#plexLibrariesTable').unblock();
+			message('Plex Share',response.message,activeInfo.settings.notifications.position,"#FFF","success","5000");
+		}catch(e) {
+			organizrCatchError(e,data);
+		}
+	}).fail(function(xhr) {
+		message('Plex Share',response.message,activeInfo.settings.notifications.position,"#FFF","error","5000");
+		$('#plexLibrariesTable').unblock();
+		OrganizrApiError(xhr);
+	});
 }
 // EVENTS and LISTENERS
