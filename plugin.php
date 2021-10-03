1568615a63e98a9007c34fec19c1e6d4c78243e8
causefx <causefx@me.com>
2021-10-03 00:07:54 -0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -20,7 +20,6 @@ class plexLibrariesPlugin extends Organizr
 {
 	public function _pluginGetSettings()
 	{
-
 		$libraryList = [['name' => 'Refresh page to update List', 'value' => '', 'disabled' => true]];
 		if ($this->config['plexID'] !== '' && $this->config['plexToken'] !== '') {
 			$libraryList = [];
@@ -29,174 +28,209 @@ class plexLibrariesPlugin extends Organizr
 				$libraryList[] = ['name' => $key, 'value' => $value];
 			}
 		}
-
+		$this->setGroupOptionsVariable();
 		return array(
-			'Plex Libraries Settings' => array(
-				array(
-					'type' => 'password-alt',
-					'name' => 'plexToken',
-					'label' => 'Plex Token',
-					'value' => $this->config['plexToken'],
-				),
-				array(
-					'type' => 'password-alt',
-					'name' => 'plexID',
-					'label' => 'Plex Machine',
-					'value' => $this->config['plexID'],
-				),
-				array(
-					'type' => 'input',
-					'name' => 'plexAdmin',
-					'label' => 'Plex Admin Username',
-					'value' => $this->config['plexAdmin'],
-				),
-				array(
-					'type' => 'select2',
-					'name' => 'PLEXLIBRARIES-librariesToInclude',
-					'label' => 'Plex Libraries to Include',
-					'value' => $this->config['PLEXLIBRARIES-librariesToInclude'],
-					'options' => $libraryList,
-				),
-			),
-			'NOTE' => array(
-				array(
-					'type' => 'html',
-					'label' => 'Note',
-					'html' => '<p>Authentication settings can be setup using the "Get Plex Token" button when selecting Plex as the Authentication backend. It also shares the same settings with the Plex homepage item.</p>
-					           <p>The Plex Admin Username is not shared with the other settings.</p>'
-				)
+			'Settings' => array(
+				$this->settingsOption('token', 'plexToken'),
+				$this->settingsOption('button', '', ['label' => 'Get Plex Token', 'icon' => 'fa fa-ticket', 'text' => 'Retrieve', 'attr' => 'onclick="PlexOAuth(oAuthSuccess,oAuthError, null, \'#PLEXLIBRARIES-settings-page [name=plexToken]\')"']),
+				$this->settingsOption('password-alt', 'plexID', ['label' => 'Plex Machine']),
+				$this->settingsOption('button', '', ['label' => 'Get Plex Machine', 'icon' => 'fa fa-id-badge', 'text' => 'Retrieve', 'attr' => 'onclick="showPlexMachineForm(\'#PLEXLIBRARIES-settings-page [name=plexID]\')"']),
+				$this->settingsOption('auth', 'PLEXLIBRARIES-pluginAuth'),
+				$this->settingsOption('input', 'plexAdmin', ['label' => 'Plex Admin Username or Email']),
+				$this->settingsOption('plex-library-include', 'PLEXLIBRARIES-librariesToInclude', ['options' => $libraryList])
 			)
 		);
 	}
-
-
-	public function getPlexShares($includeAll = false,$userId = "") {
-		if (!empty($this->config['plexToken'])) {
-			$headers = array(
-				'Content-type: application/xml',
-				'X-Plex-Token' => $this->config['plexToken'],
-			);
-			$url = 'https://plex.tv/api/users';
+	
+	public function _pluginLaunch()
+	{
+		$user = $this->getUserById($this->user['userID']);
+		if ($user) {
+			if ($user['plex_token'] !== null) {
+				$this->setResponse(200, 'User approved for plugin');
+				return true;
+			}
+		}
+		$this->setResponse(401, 'User not approved for plugin');
+		return false;
+	}
+	
+	public function plexLibrariesPluginGetPlexShares($includeAll = false, $userId = "")
+	{
+		if (empty($this->config['plexToken'])) {
+			$this->setResponse(409, 'plexToken is not setup');
+			return false;
+		}
+		$headers = array(
+			'Content-type: application/xml',
+			'X-Plex-Token' => $this->config['plexToken'],
+		);
+		// Check if user is Plex Admin
+		if ((strtolower($this->user['username']) == strtolower($this->config['plexAdmin']) || strtolower($this->user['email']) == strtolower($this->config['plexAdmin'])) && !$userId) {
+			$url = 'https://plex.tv/api/servers/' . $this->config['plexID'] . '/shared_servers/';
 			try {
-				$response = Requests::get($url, $headers, array());
+				$response = Requests::get($url, $headers, []);
 				if ($response->success) {
 					libxml_use_internal_errors(true);
-					$userXML = simplexml_load_string($response->body);
+					$plex = simplexml_load_string($response->body);
+					$libraryList = array();
+					foreach ($plex->SharedServer as $child) {
+						if (!empty($child['username'])) {
+							$libraryList[(string)$child['username']]['username'] = (string)$child['username'];
+							$libraryList[(string)$child['username']]['email'] = (string)$child['email'];
+							$libraryList[(string)$child['username']]['id'] = (string)$child['id'];
+							$libraryList[(string)$child['username']]['userID'] = (string)$child['userID'];
+							foreach ($child->Section as $library) {
+								$library = current($library->attributes());
+								$libraryList[(string)$child['username']]['libraries'][] = $library;
+							}
+						}
+					}
+					$libraryList = array_change_key_case($libraryList, CASE_LOWER);
+					ksort($libraryList);
+					$apiData = [
+						'plexAdmin' => true,
+						'libraryData' => $libraryList
+					];
+					$this->setResponse(200, null, $apiData);
+					return $apiData;
+				} else {
+					$this->setResponse(500, 'Plex error');
+					return false;
 				}
 			} catch (Requests_Exception $e) {
 				$this->writeLog('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 'SYSTEM');
 				$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 400);
 				return false;
 			}
-
-		    $Username = $this->user['username'];
-			if ($Username == $this->config['plexAdmin'] && !$userId) {
-				$url = 'https://plex.tv/api/servers/'.$this->config['plexID'].'/shared_servers/';
-				try {
-					$response = Requests::get($url, $headers, array());
-					if ($response->success) {
-						libxml_use_internal_errors(true);
-						$shareXML = simplexml_load_string($response->body);
-					}
-					return $shareXML;
-				} catch (Requests_Exception $e) {
-					$this->writeLog('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 'SYSTEM');
-					$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 400);
-					return false;
-				}
-				return false;
+		} else {
+			$searchTerm = ($userId) ?: $this->user['email'];
+			$searchKey = ($userId) ? 'shareId' : 'email';
+			$plexUsers = $this->allPlexUsers(false, true);
+			$key = array_search($searchTerm, array_column($plexUsers, $searchKey));
+			if ($key !== false) {
+				$url = 'https://plex.tv/api/servers/' . $this->config['plexID'] . '/shared_servers/' . $plexUsers[$key]['shareId'];
 			} else {
-				$UserData = $userXML->xpath('//User[@username="'.$Username.'"]');
-				if ($userId) {
-					$url = 'https://plex.tv/api/servers/'.$this->config['plexID'].'/shared_servers/'.$userId;	
-				} else {
-					$url = 'https://plex.tv/api/servers/'.$this->config['plexID'].'/shared_servers/'.$UserData[0]->Server->attributes()->id;
-				}
-				try {
-					$response = Requests::get($url, $headers, array());
-					if ($response->success) {
-						libxml_use_internal_errors(true);
-						$shareXML = simplexml_load_string($response->body);
-						if (!$includeAll) {
-							$librariesToInclude = explode(",",$this->config['PLEXLIBRARIES-librariesToInclude']);
-							foreach ($shareXML->xpath('//Section') as $share) {
-								if (!in_array($share->attributes()->key, $librariesToInclude)) {
-									$key = $share->attributes()->key;
-									unset($share[0]);
+				$this->setResponse(404, 'User Id was not found in Plex Users');
+				return false;
+			}
+			try {
+				$response = Requests::get($url, $headers, array());
+				if ($response->success) {
+					libxml_use_internal_errors(true);
+					$plex = simplexml_load_string($response->body);
+					$libraryList = array();
+					foreach ($plex->SharedServer as $child) {
+						if (!empty($child['username'])) {
+							$libraryList[(string)$child['username']]['username'] = (string)$child['username'];
+							$libraryList[(string)$child['username']]['email'] = (string)$child['email'];
+							$libraryList[(string)$child['username']]['id'] = (string)$child['id'];
+							$libraryList[(string)$child['username']]['shareId'] = (string)$plexUsers[$key]['shareId'];
+							foreach ($child->Section as $library) {
+								$library = current($library->attributes());
+								if (!$includeAll) {
+									$librariesToInclude = explode(',', $this->config['PLEXLIBRARIES-librariesToInclude']);
+									if (in_array($library['key'], $librariesToInclude)) {
+										$libraryList[(string)$child['username']]['libraries'][] = $library;
+									}
+								} else {
+									$libraryList[(string)$child['username']]['libraries'][] = $library;
 								}
 							}
 						}
 					}
-					return $shareXML;
-				} catch (Requests_Exception $e) {
-					$this->writeLog('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 'SYSTEM');
-					$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 400);
+					$libraryList = array_change_key_case($libraryList, CASE_LOWER);
+					$apiData = [
+						'plexAdmin' => false,
+						'libraryData' => $libraryList
+					];
+					$this->setResponse(200, null, $apiData);
+					return $apiData;
+				} else {
+					$this->setResponse(500, 'Plex Error', $response->body);
 					return false;
 				}
+				return $shareXML;
+			} catch (Requests_Exception $e) {
+				$this->writeLog('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 'SYSTEM');
+				$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 400);
 				return false;
 			}
 		}
 	}
-
-    public function call_endpoint($url, $method = 'GET', $args = false)
-    {
-        $postdata = ($args) ? json_encode($args) : '';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: '.strlen($postdata)));
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $response = curl_exec($ch);
-		return $response;
-        curl_close($ch);
-    }
-
-	public function updatePlexShares($data) {
-		$shareData = explode(":", $data["shareData"]);
-		$shareId = $shareData[0];
-		$userId = $shareData[1];
-		if ($data["checked"] == "checked") {
-			$Shares = $this->getPlexShares(true,$userId);
-			$xpath = $Shares->xpath('//SharedServer//Section[@shared="1"]');
-			$NewShares = array();
-			foreach ($xpath as $Share) {
-				$ShareString = (string)$Share->attributes()->id;
-				$NewShares[] = $ShareString;
-			}
-			if (!in_array($shareId, $NewShares)) {
-				$NewShares[] = $shareId;
-			}
-			$Msg = "Enabling ".$shareId." on user: ".$userId;
-		} else {
-			$Shares = $this->getPlexShares(true,$userId);
-			$xpath = $Shares->xpath('//SharedServer//Section[@shared="1"]');
-			$NewShares = array();
-			foreach ($xpath as $Share) {
-				$ShareString = (string)$Share->attributes()->id;
-				if ($ShareString != $shareId) {
-					$NewShares[] = $ShareString;
+	
+	public function plexLibrariesPluginUpdatePlexShares($userId, $action, $shareId)
+	{
+		if (!$userId) {
+			$this->setResponse(409, 'User Id not supplied');
+			return false;
+		}
+		if (!$action) {
+			$this->setResponse(409, 'Action not supplied');
+			return false;
+		}
+		if (!$shareId) {
+			$this->setResponse(409, 'Share Id not supplied');
+			return false;
+		}
+		$Shares = $this->plexLibrariesPluginGetPlexShares(true, $userId);
+		$NewShares = array();
+		if ($Shares) {
+			if (isset($Shares['libraryData'])) {
+				foreach ($Shares['libraryData'] as $key => $Share) {
+					foreach ($Share['libraries'] as $library) {
+						if ($library['shared'] == 1) {
+							$ShareString = (string)$library['id'];
+							if ($action == 'share') {
+								$NewShares[] = $ShareString;
+								$Msg = 'Enabled share';
+							} else {
+								$Msg = 'Disabled share';
+								if ($ShareString !== $shareId) {
+									$NewShares[] = $ShareString;
+								}
+							}
+						}
+					}
+				}
+				if ($action == 'share') {
+					if (!in_array($shareId, $NewShares)) {
+						$NewShares[] = $shareId;
+					}
 				}
 			}
-			$Msg = "Disabling ".$shareId." on user: ".$userId;
 		}
 		if (empty($NewShares)) {
 			$this->setAPIResponse('error', 'You must have at least one share.', 400);
+			return false;
 		} else {
-			$http_body = array(
+			$http_body = [
 				"server_id" => $this->config['plexID'],
-				"shared_server" => array(
+				"shared_server" => [
 					"library_section_ids" => $NewShares
-			));
+				]
+			];
 			if ($userId) {
-				$url = 'https://plex.tv/api/servers/'.$this->config['plexID'].'/shared_servers/'.$userId.'?X-Plex-Token='.$this->config['plexToken'];
-			} else {
-				$url = 'https://plex.tv/api/servers/'.$this->config['plexID'].'/shared_servers/'.$Shares->SharedServer->attributes()->id.'?X-Plex-Token='.$this->config['plexToken'];
+				$url = 'https://plex.tv/api/servers/' . $this->config['plexID'] . '/shared_servers/' . $userId . '?X-Plex-Token=' . $this->config['plexToken'];
 			}
-			$response = $this->call_endpoint($url, "PUT", $http_body);
-			$this->setAPIResponse('success', $Msg, 200);
+			$headers = [
+				'Accept' => 'application/json',
+				'Content-Type' => 'application/json',
+			];
+			try {
+				$response = Requests::put($url, $headers, json_encode($http_body), []);
+				if ($response->success) {
+					$this->setAPIResponse('success', $Msg, 200, $http_body);
+					return $http_body;
+				} else {
+					$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: Plex Error', 400);
+					return false;
+				}
+			} catch (Requests_Exception $e) {
+				$this->writeLog('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 'SYSTEM');
+				$this->setAPIResponse('error', 'PlexLibraries Plugin - Error: ' . $e->getMessage(), 400);
+				return false;
+			}
 		}
 	}
 }
